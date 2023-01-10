b7c00195a37ced4d027a10b631f4450b70059840
Killbazz <120661184+Killbazz@users.noreply.github.com>
2023-01-10 11:06:31 -0300

    Add files via upload

---
+++ poo/Funcionario.java
@@ -0,0 +1,66 @@
+package poo;
+
+public class Funcionario {
+	
+	private String funcionarioContratar;
+    private String setorAtuacao;
+    private int horasMensais;
+    private String salario;
+    
+    // método construtor
+
+	public Funcionario(String funcionarioContratar, String setorAtuacao, int horasMensais, String salario) {
+		this.funcionarioContratar = funcionarioContratar;
+		this.setorAtuacao = setorAtuacao;
+		this.horasMensais = horasMensais;
+		this.salario = salario;
+	}
+
+
+	public String getFuncionarioContratar() {
+		return funcionarioContratar;
+	}
+
+
+	public void setFuncionarioContratar(String funcionarioContratar) {
+		this.funcionarioContratar = funcionarioContratar;
+	}
+
+
+	public String getSetorAtuacao() {
+		return setorAtuacao;
+	}
+
+
+	public void setSetorAtuacao(String setorAtuacao) {
+		this.setorAtuacao = setorAtuacao;
+	}
+
+
+	public int getHorasMensais() {
+		return horasMensais;
+	}
+
+
+	public void setHorasMensais(int horasMensais) {
+		this.horasMensais = horasMensais;
+	}
+
+
+	public String getSalario() {
+		return salario;
+	}
+
+
+	public void setSalario(String salario) {
+		this.salario = salario;
+	}
+    
+	public void imprimirInfo() {
+	
+		  System.out.println("\nNome: "+funcionarioContratar+" \nProfissão: " +setorAtuacao+" \nHoras trabahadas: "+horasMensais+  " \nhoras Salário: " +salario+"");
+	}
+    
+}
+
+}
