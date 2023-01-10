a943c3d0bada82aa3f57a3cc8867047032a4cc9d
Killbazz <120661184+Killbazz@users.noreply.github.com>
2023-01-10 11:18:12 -0300

    Update Funcionario.java

--- poo/Funcionario.java
+++ poo/Funcionario.java
@@ -65,3 +65,51 @@ public class Funcionario {
 }
 
 }
+
+package poo;
+
+public class FuncionarioContratado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Funcionario contratado = new Funcionario ("Marlon Bassoto","Desenvolvedor Júnior",36,"3390");
+		contratado.imprimirInfo();
+		
+		System.out.println("\nFuncionario Promovida");
+		
+		
+		System.out.println("\nVaga da promoção");
+		contratado.setSetorAtuacao("Desenvolvedor Full Stack Pleno");
+		//contratado.imprimirInfo();
+		System.out.println(contratado.getSetorAtuacao());
+				
+		System.out.println("\nAlteração Sálario ");
+		System.out.println();
+		contratado.setSalario("10.000");
+		//contratado.imprimirInfo();
+		System.out.println("Para:R$ "+contratado.getSalario());
+		
+		contratado.imprimirInfo();
+
+		
+		
+		
+		
+	
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+	
+		
+
+	}
+
+}
